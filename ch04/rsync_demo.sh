2914e48f4201f31cec5da16999153a78cab6dfcc
BrentD09 <brent.denny@lumifygroup.com>
2025-07-29 13:17:32 +1000

    Update rsync_demo.sh

--- ch04/rsync_demo.sh
+++ ch04/rsync_demo.sh
@@ -1,5 +1,5 @@
 mkdir ~/rsync_files 2> /dev/null
 echo $(yes "this is a demo for rsync" | head -n 200) > ~/rsync_files/rsync_demo.txt
 
-rsync -a ~/rsync_files servera:/home/student/rsync_data1
-rsync -a ~/rsync_files/ servera:/home/student/rsync_data2
+rsync -av ~/rsync_files servera:/home/student/rsync_data1
+rsync -av ~/rsync_files/ servera:/home/student/rsync_data2
