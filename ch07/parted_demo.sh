e5cd0b5970c1e00526eba0e401da83661867e12b
BrentD09 <brent.denny@lumifygroup.com>
2025-07-30 08:39:33 +1000

    Update parted_demo.sh

--- ch07/parted_demo.sh
+++ ch07/parted_demo.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 lsblk
+parted /dev/vdd
+print
+mklabel gpt
+print
+mkpart
+first
+xfs
+0MiB
+2000MiB
+Y
+i
+print
+quit
+lsblk
+
+parted /dev/vdd
+print 
+mkpart second xfs 2001mib 3000mib
+print 
